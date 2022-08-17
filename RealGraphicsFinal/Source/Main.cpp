72b6cacd67dc13a1f1d1243fb04e4167e638d783
Eddie Krystowski <eddiekrystowski@gmail.com>
2022-08-17 10:47:53 -0400

    add resize back

--- RealGraphicsFinal/Source/Main.cpp
+++ RealGraphicsFinal/Source/Main.cpp
@@ -1107,6 +1107,8 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     // make sure the viewport matches the new window dimensions; note that width and 
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
+    Window::Width = width;
+    Window::Height = height;
 }
 
 // glfw: whenever the mouse moves, this callback is called
