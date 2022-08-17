4ed6d73147125019aa9a4131dda1dca839492eb4
Eddie Krystowski <eddiekrystowski@gmail.com>
2022-08-17 03:38:20 -0400

    add basic skybox

--- RealGraphicsFinal/Shaders/skybox.fs
+++ RealGraphicsFinal/Shaders/skybox.fs
@@ -1,15 +1,16 @@
-#version 330 core
+#version 430 core
 out vec4 FragColor;
 
-uniform vec4 color;
+uniform samplerCube skybox;
 uniform float fogStart;
 uniform float fogEnd;
 uniform vec4 fogColor;
 
 in vec4 FragPos;
+in vec3 TexCoords;
 
 void main()
 {   
     float fog = clamp(((fogEnd - length(FragPos.xyz)) / (fogEnd - fogStart)), 0.0, 1.0);
-    FragColor = mix(fogColor, color, fog);
-}
+    FragColor = mix(fogColor, texture(skybox, TexCoords).rgba, fog);
+}   
--- RealGraphicsFinal/Source/Main.cpp
+++ RealGraphicsFinal/Source/Main.cpp
@@ -1411,6 +1411,45 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
     }
 }
 
+// loads a cubemap texture from 6 individual texture faces
+// order:
+// +X (right)
+// -X (left)
+// +Y (top)
+// -Y (bottom)
+// +Z (front) 
+// -Z (back)
+// -------------------------------------------------------
+unsigned int loadCubemap(std::vector<std::string> faces)
+{
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+
+    int width, height, nrChannels;
+    for (unsigned int i = 0; i < faces.size(); i++)
+    {
+        unsigned char* data = stbi_load(faces[i].c_str(), &width, &height, &nrChannels, 0);
+        if (data)
+        {
+            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+            stbi_image_free(data);
+        }
+        else
+        {
+            std::cout << "Cubemap texture failed to load at path: " << faces[i] << std::endl;
+            stbi_image_free(data);
+        }
+    }
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+    return textureID;
+}
+
 
 void renderScene(Shader* shader, Shader* directionalShader, unsigned int cubeTexture, WaterFrameBuffer* waterFrameBuffer, Water* water, Terrain* terrain, Model* castle, Shader& treeShader, Model* tree);
 void renderCube(unsigned int texture, bool clipPlaneEnabled, glm::vec4 clipPlane = glm::vec4(0, 0, 0, 0));
@@ -1477,6 +1516,9 @@ int main()
     Shader* tessHeightMapGrassShader = new Shader("./Shaders/passthrough.vs", "./Shaders/grass.fs", "./Shaders/grass.gs", "./Shaders/tessellation.tesc", "./Shaders/tessellation.tese");
     Shader* treeShader = new Shader("./Shaders/treeShader.vs", "./Shaders/directional.fs", nullptr, nullptr, nullptr);
     Shader* simpleDepthShader = new Shader("./Shaders/depthShader.vs", "./Shaders/depthShader.fs", nullptr, nullptr, nullptr);
+    Shader* skyboxShader = new Shader("./Shaders/skybox.vs", "./Shaders/skybox.fs");
+
+
 
     tessHeightMapGrassShader->use();
     tessHeightMapGrassShader->setInt("grass_texture", 0);
@@ -1540,7 +1582,6 @@ int main()
     unsigned int cliffTexture = loadTexture("Textures/cliff.jpg");
     unsigned int windMap = loadTexture("Textures/wind.jpg");
     unsigned int cliffNormalMap = loadTexture("Textures/cliffBump.png");
-    unsigned int castleWallTexture = loadTexture("Textures/walltexture.jpg");
     castleNormal = loadTexture("Textures/castleNormal5.jpg");
 
     stbi_set_flip_vertically_on_load(false);
@@ -1591,6 +1632,73 @@ int main()
     glDrawBuffer(GL_NONE);
     glReadBuffer(GL_NONE);
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    
+    // configure skybox
+    float skyboxVertices[] = {
+        // positions          
+        -1.0f,  1.0f, -1.0f,
+        -1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+         1.0f,  1.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f,
+
+        -1.0f, -1.0f,  1.0f,
+        -1.0f, -1.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f,
+        -1.0f,  1.0f,  1.0f,
+        -1.0f, -1.0f,  1.0f,
+
+         1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+
+        -1.0f, -1.0f,  1.0f,
+        -1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f, -1.0f,  1.0f,
+        -1.0f, -1.0f,  1.0f,
+
+        -1.0f,  1.0f, -1.0f,
+         1.0f,  1.0f, -1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+        -1.0f,  1.0f,  1.0f,
+        -1.0f,  1.0f, -1.0f,
+
+        -1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f,  1.0f,
+         1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f,  1.0f,
+         1.0f, -1.0f,  1.0f
+    };
+
+    unsigned int skyboxVAO, skyboxVBO;
+    glGenVertexArrays(1, &skyboxVAO);
+    glGenBuffers(1, &skyboxVBO);
+    glBindVertexArray(skyboxVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+    std::vector<std::string> faces
+    {
+        "./Textures/skybox/right.jpg",
+        "./Textures/skybox/left.jpg",
+        "./Textures/skybox/top.jpg",
+        "./Textures/skybox/bottom.jpg",
+        "./Textures/skybox/front.jpg",
+        "./Textures/skybox/back.jpg"
+    };
+    unsigned int skybox = loadCubemap(faces);
  
     // load models
     Model tree = Model("./Models/treeModelLowPoly.obj");
@@ -1690,6 +1798,22 @@ int main()
         }
        
 
+        // draw skybox as last
+        glDepthFunc(GL_LEQUAL);  // change depth function so depth test passes when values are equal to depth buffer's content
+        skyboxShader->use();
+        glm::mat4 view = glm::mat4(glm::mat3(camera->GetViewMatrix())); // remove translation from the view matrix
+        skyboxShader->setMat4("view", view);
+        skyboxShader->setMat4("projection", camera->GetProjectionMatrix());
+        skyboxShader->setVec4("fogColor", glm::vec4(ImguiHelper::fogColor[0], ImguiHelper::fogColor[1], ImguiHelper::fogColor[2], ImguiHelper::fogColor[3]) );
+        skyboxShader->setFloat("fogStart", ImguiHelper::fogStart);
+        skyboxShader->setFloat("fogEnd", ImguiHelper::fogEnd);
+        // skybox cube
+        glBindVertexArray(skyboxVAO);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_CUBE_MAP, skybox);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glBindVertexArray(0);
+        glDepthFunc(GL_LESS); // set depth function back to default
         
 
         glCheckError();
